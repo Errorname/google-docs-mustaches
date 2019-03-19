cda4c1fac5df4d0a7e8263b1815b33171a09dd73
Maxim Gladkov <contact@maximgladkov.com>
2019-03-19 14:46:45 +0200

    add support for tables as well

--- lib/interpolation/interpolate.d.ts
+++ lib/interpolation/interpolate.d.ts
@@ -1,4 +1,4 @@
-import { GDoc, Request } from './types';
-import { Formatters } from '../types';
+import { GDoc, Request } from "./types";
+import { Formatters } from "../types";
 declare const interpolate: (doc: GDoc, data: any, formatters: Formatters) => Request[];
 export default interpolate;
--- src/interpolation/interpolate.ts
+++ src/interpolation/interpolate.ts
@@ -1,44 +1,64 @@
-import { GDoc, Request } from './types'
-import { Formatters } from '../types'
+import { GDoc, Request } from "./types";
+import { Formatters } from "../types";
 
-import dot from './dot'
+import dot from "./dot";
 
-const interpolate = (doc: GDoc, data: any, formatters: Formatters): Request[] => {
-  const placeholders = findPlaceholders(doc)
-  return computeUpdates(placeholders, data, formatters)
-}
+const interpolate = (
+  doc: GDoc,
+  data: any,
+  formatters: Formatters
+): Request[] => {
+  const placeholders = findPlaceholders(doc);
+  return computeUpdates(placeholders, data, formatters);
+};
 
 const findPlaceholders = (doc: GDoc): string[] => {
-  const placeholders: string[] = []
+  const placeholders: string[] = [];
 
-  doc.body.content.map(c => {
-    if (c.paragraph) {
-      c.paragraph.elements.map(e => {
-        if (e.textRun) {
-          const matches = e.textRun.content.match(/{{([^}]*)}}/gi) || []
-          matches.map(m => placeholders.push(m.slice(2, -2)))
-        }
-      })
-    }
-  })
+  const processContent = (content: any[]) => {
+    content.map(c => {
+      if (c.paragraph) {
+        c.paragraph.elements.map((e: any) => {
+          if (e.textRun) {
+            const matches = e.textRun.content.match(/{{([^}]*)}}/gi) || [];
+            matches.map((m: any) => placeholders.push(m.slice(2, -2)));
+          }
+        });
+      }
 
-  return placeholders
-}
+      if (c.table) {
+        c.table.tableRows.map((r: any) => {
+          r.tableCells.map((c: any) => {
+            processContent(c.content);
+          });
+        });
+      }
+    });
+  };
+
+  processContent(doc.body.content);
+
+  return placeholders;
+};
 
 const availableFormatters: Formatters = {
   lowercase: (s: string) => s.toLowerCase(),
   uppercase: (s: string) => s.toUpperCase()
-}
+};
 
-const computeUpdates = (placeholders: string[], data: any, formatters: Formatters): Request[] => {
-  formatters = { ...availableFormatters, ...formatters }
+const computeUpdates = (
+  placeholders: string[],
+  data: any,
+  formatters: Formatters
+): Request[] => {
+  formatters = { ...availableFormatters, ...formatters };
 
   const replacements = placeholders.map(
     (placeholder): [string, string] => {
-      const computed: string = `${dot(data, placeholder, { formatters })}`
-      return [placeholder, computed]
+      const computed: string = `${dot(data, placeholder, { formatters })}`;
+      return [placeholder, computed];
     }
-  )
+  );
 
   return replacements.map(([placeholder, computed]) => ({
     replaceAllText: {
@@ -48,7 +68,7 @@ const computeUpdates = (placeholders: string[], data: any, formatters: Formatter
         matchCase: false
       }
     }
-  }))
-}
+  }));
+};
 
-export default interpolate
+export default interpolate;
