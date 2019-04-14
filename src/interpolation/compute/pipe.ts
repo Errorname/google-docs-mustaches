a13fd392b397b64f84dd5a0d864a423c1c3e7f6e
Antoine Carat <antoine.carat@gmail.com>
2019-04-14 12:30:08 +0100

    Cleanup

--- src/interpolation/compute/pipe.ts
+++ src/interpolation/compute/pipe.ts
@@ -10,48 +10,51 @@ export default (
   options?: { formatters?: Formatters; fallback?: string }
 ): any => {
   const formatters: Formatters = { ...defaultFormatters, ...(options && options.formatters) }
-    const matches = transformation.match(/^(?<formatter>[^\(]+)(\((?<params>.+)?\))?/)
+  const matches = transformation.match(/^(?<formatter>[^\(]+)(\((?<params>.+)?\))?/)
 
-    if (matches && matches.groups && matches.groups.formatter) {
-      const formatterName = matches.groups.formatter.toLowerCase()
-      const formatter = formatters[formatterName]
-      if (formatter) {
-        let typedParams = []
-        if (matches.groups.params) {
-          try {
-            typedParams = matches.groups.params
-              .split(',')
-              .map(e => e.trim())
-              .map(e => {
-                if (/^(\“.*\”|\".*\"|\‘.*\’|\'.*\')$/.test(e)) { // String
-                  return e.slice(1, -1)
-                } else if (/^[-+]?([0-9]+|[0-9]+\.[0-9]*|[0-9]*\.[0-9]+)$/.test(e)) { // Number
-                  return Number(e)
-                } else if (/^true|false$/.test(e)) { //Boolean
-                  return Boolean(e)
-                } else { // Variable
-                  try {
-                    return dot(e, data)
-                  } catch (err) {
-                    if ((options && options.fallback) === undefined) {
-                      throw err
-                    } else {
-                      return options && options.fallback
-                    }
-                  }
-                }
-              }
-            )
-          } catch (err) { // Ignore the formatter if unknown variable (w/o fallback) in params
-            return value
+  if (!matches || !matches.groups || !matches.groups.formatter) {
+    throw new UnvalidFormatterError(`${transformation} is not a valid formatter.`)
+  }
+
+  const formatterName = matches.groups.formatter.toLowerCase()
+  const formatter = formatters[formatterName]
+
+  if (!formatter) {
+    throw new UnknownFormatterError(`${formatterName} is not defined.`)
+  }
+
+  let typedParams = []
+  if (matches.groups.params) {
+    try {
+      typedParams = normalizeParams(matches.groups.params.split(','), data, options)
+    } catch (err) { // Ignore the formatter if unknown variable (w/o fallback) in params
+      return value
+    }
+  }
+
+  return formatter(value, ...typedParams)
+}
+
+const normalizeParams = function(untypedParams: string[], data: any, options?: { formatters?: Formatters; fallback?: string }) {
+  return untypedParams.map(e => e.trim())
+    .map(e => {
+      if (/^(\“.*\”|\".*\"|\‘.*\’|\'.*\')$/.test(e)) { // String
+        return e.slice(1, -1)
+      } else if (/^[-+]?([0-9]+|[0-9]+\.[0-9]*|[0-9]*\.[0-9]+)$/.test(e)) { // Number
+        return Number(e)
+      } else if (/^true|false$/.test(e)) { //Boolean
+        return Boolean(e)
+      } else { // Variable
+        try {
+          return dot(e, data)
+        } catch (err) {
+          if ((options && options.fallback) === undefined) {
+            throw err
+          } else {
+            return options && options.fallback
           }
         }
-        value = formatter(value, ...typedParams)
-      } else {
-        throw new UnknownFormatterError(`${formatterName} is not defined.`)
       }
-    } else {
-      throw new UnvalidFormatterError(`${transformation} is not a valid formatter.`)
     }
-  return value
+  )
 }
