cb2558f7ad44d6c5304b16d8836a4ca1fe9644c3
Thibaud Courtoison <do.not.press.enter@gmail.com>
2019-04-19 14:56:24 +0200

    :wrench: Refact token script to allow refresh (#21)

--- dev/token.js
+++ dev/token.js
@@ -1,5 +1,5 @@
-const readline = require('readline')
 const fs = require('fs')
+const readline = require('readline')
 const { google } = require('googleapis')
 const credentials = require('./credentials.json')
 
@@ -7,40 +7,80 @@ const SCOPES = [
   'https://www.googleapis.com/auth/drive',
   'https://www.googleapis.com/auth/documents'
 ]
+
 const TOKEN_PATH = 'dev/token.json'
 
-function authorize(credentials) {
+const readFile = path =>
+  new Promise((resolve, reject) =>
+    fs.readFile(path, (err, content) => {
+      if (err) return reject(err)
+      resolve(JSON.parse(content))
+    })
+  )
+
+const writeFile = (path, json) =>
+  new Promise((resolve, reject) =>
+    fs.writeFile(path, JSON.stringify(json), err => {
+      if (err) return reject(err)
+      resolve()
+    })
+  )
+
+const askQuestion = text =>
+  new Promise(resolve => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+    rl.question(text, code => {
+      rl.close()
+      resolve(code)
+    })
+  })
+
+const getNewToken = oAuth2Client =>
+  new Promise(async resolve => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    })
+    console.log('Authorize this app by visiting this url:', authUrl)
+
+    const code = await askQuestion('Enter the code from that page here: ')
+
+    oAuth2Client.getToken(code, async (err, token) => {
+      if (err) return console.error('Error retrieving access token', err)
+      console.log(token)
+      oAuth2Client.setCredentials(token)
+      await writeFile(TOKEN_PATH, token)
+      console.log('Token stored to', TOKEN_PATH)
+      resolve(token)
+    })
+  })
+
+const refreshToken = (oAuth2Client, token) =>
+  new Promise(async resolve => {
+    oAuth2Client.setCredentials(token)
+    const accessToken = (await oAuth2Client.getRequestHeaders()).Authorization.split(' ')[1]
+    token.access_token = accessToken
+    await writeFile(TOKEN_PATH, token)
+    console.log('Token refreshed')
+    resolve()
+  })
+
+;(async () => {
   const { client_secret, client_id, redirect_uris } = credentials.installed
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
 
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client)
-    oAuth2Client.setCredentials(JSON.parse(token))
-    console.log('\nYou can now use the playground: npm run playground')
-  })
-}
+  let token
+  try {
+    token = await readFile(TOKEN_PATH)
+    oAuth2Client.setCredentials(token)
+    refreshToken(oAuth2Client, token)
+  } catch (err) {
+    console.error(err)
+    token = await getNewToken(oAuth2Client)
+  }
 
-function getNewToken(oAuth2Client) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  })
-  console.log('Authorize this app by visiting this url:', authUrl)
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  rl.question('Enter the code from that page here: ', code => {
-    rl.close()
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err)
-      oAuth2Client.setCredentials(token)
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) return console.error(err)
-        console.log('Token stored to', TOKEN_PATH)
-      })
-    })
-  })
-}
-
-authorize(credentials)
+  console.log('\nYou can now use the playground: npm run playground')
+})()
