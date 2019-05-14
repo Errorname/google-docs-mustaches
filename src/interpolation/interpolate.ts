a23e01aed41581168eb0491978063d43b68501c5
Thibaud Courtoison <do.not.press.enter@gmail.com>
2019-05-14 10:12:24 +0200

    :sparkles: Implements images placeholders (#23)

--- src/interpolation/interpolate.ts
+++ src/interpolation/interpolate.ts
@@ -1,4 +1,5 @@
-import { GDoc, Request, Formatters, Placeholder, ContentPlaceholder } from './types'
+import { Formatters, Placeholder, ContentPlaceholder } from './types'
+import { GDoc, Request, Unit } from './gdocTypes'
 
 import computeContent from './compute'
 
@@ -66,8 +67,40 @@ const buildUpdates = (placeholders: Placeholder[]): Request[] => {
     // ContentPlaceholder
     if (placeholder.type == 'content') {
       const contentPlaceholder = placeholder as ContentPlaceholder
+      // Image
+      if (contentPlaceholder.output.url) {
+        const output = contentPlaceholder.output
+        // Insert image
+        updates.push({
+          insertInlineImage: {
+            uri: output.url,
+            objectSize: {
+              width: output.width ? { magnitude: output.width, unit: Unit.PT } : undefined,
+              height: output.height ? { magnitude: output.height, unit: Unit.PT } : undefined
+            },
+            location: {
+              segmentId: '', // Empty means body
+              index: placeholder.position.start
+            }
+          }
+        })
+        // Remove mustaches
+        updates.push({
+          replaceAllText: {
+            replaceText: '',
+            containsText: {
+              text: placeholder.raw,
+              matchCase: true
+            }
+          }
+        })
+      }
+
       // Text
-      if (contentPlaceholder.output === `${contentPlaceholder.output}`) {
+      if (
+        contentPlaceholder.output === `${contentPlaceholder.output}` ||
+        !isNaN(contentPlaceholder.output)
+      ) {
         updates.push({
           replaceAllText: {
             replaceText: `${contentPlaceholder.output}`,
@@ -93,6 +126,17 @@ const buildUpdates = (placeholders: Placeholder[]): Request[] => {
       })
     }
   }
+
+  // Sort updates
+  /* For Google Doc, updates are applied one after the other. This means that the origin
+  position of placeholders may change if a previous update was applied, (eg: with a long text)!
+  We need a better dynamic position system to handle this, but in the meantime,
+  we simply sort the imageUpdates first to mitigate the problem. */
+  updates.sort((a, b) => {
+    if (a.insertInlineImage && !b.insertInlineImage) return -1
+    if (!a.insertInlineImage && b.insertInlineImage) return 1
+    return 0
+  })
 
   return updates
 }
