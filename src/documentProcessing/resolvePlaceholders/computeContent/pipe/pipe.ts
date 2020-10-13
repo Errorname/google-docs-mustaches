234d02214a6078865521c35f66e08316a95f4833
8BitJonny <timon99@gmx.de>
2020-10-13 13:50:45 +0200

    :bug: Fix wrong formatter param splitting

--- src/documentProcessing/resolvePlaceholders/computeContent/pipe/pipe.ts
+++ src/documentProcessing/resolvePlaceholders/computeContent/pipe/pipe.ts
@@ -25,7 +25,7 @@ export default (
 
   let typedParams = []
   if (matches.groups.params) {
-    typedParams = normalizeParams(matches.groups.params.split(','), data, options)
+    typedParams = normalizeParams(matches.groups.params.split(/,(?=(?:[^“”"']|[“"'][^“”"']*[”"'])*$)/), data, options)
   }
 
   return formatter(value, ...typedParams)
